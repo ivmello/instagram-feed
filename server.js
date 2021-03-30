bc323df6fd3a525eed59f72a00e4aca4e5182a4b
Igor Vieira de Mello <igor@shapeweb.com.br>
2021-03-30 10:04:08 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const instagram = require('instagram-scraping');
+
+const app = express();
+const router = express.Router();
+const PORT = 3000;
+
+router.get('/user/:user', function(req, res) {
+    const user = req.params.user;
+    instagram.scrapeUserPage(user).then(result => {
+        res.json(result);
+    });
+});
+
+app.use(router);
+
+app.listen(PORT, function(err) {
+    console.log(`running on PORT ${PORT}`);
+});
